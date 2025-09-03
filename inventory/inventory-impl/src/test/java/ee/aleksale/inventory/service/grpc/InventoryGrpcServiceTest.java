fbf874bb17e4877f54dd6f20e77df56350541920
aleksale <aleksandr.aleksandrov@tietoevry.com>
2025-09-03 12:22:57 +0300

    [AA] - Inventory Grpc Service unit

---
+++ inventory/inventory-impl/src/test/java/ee/aleksale/inventory/service/grpc/InventoryGrpcServiceTest.java
@@ -0,0 +1,121 @@
+package ee.aleksale.inventory.service.grpc;
+
+import ee.aleksale.common.proto.v1.CommerceResponse;
+import ee.aleksale.common.proto.v1.InventoryUnit;
+import ee.aleksale.inventory.exception.InventoryException;
+import ee.aleksale.inventory.service.InventoryService;
+import io.grpc.internal.testing.StreamRecorder;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+
+@ExtendWith(MockitoExtension.class)
+class InventoryGrpcServiceTest {
+
+    private InventoryService inventoryService;
+
+    private InventoryGrpcService inventoryGrpcService;
+
+    @BeforeEach
+    void init() {
+        inventoryService = mock(InventoryService.class);
+
+        inventoryGrpcService = new InventoryGrpcService(
+                inventoryService
+        );
+    }
+
+    @Test
+    void addInventoryUnit_success() {
+        var responseObserver = StreamRecorder.<CommerceResponse>create();
+        var requestObserver = inventoryGrpcService.addInventoryUnit(responseObserver);
+
+        var unit = InventoryUnit.getDefaultInstance();
+        doReturn(unit)
+                .when(inventoryService)
+                .saveInventory(unit);
+
+        requestObserver.onNext(unit);
+
+        assertFalse(responseObserver.getValues().isEmpty());
+        assertEquals(1, responseObserver.getValues().size());
+
+        var commerceResponse = responseObserver.getValues().get(0);
+        assertNotNull(commerceResponse.getSuccess());
+
+        assertFalse(commerceResponse.getSuccess().getDataList().isEmpty());
+        assertEquals(1, commerceResponse.getSuccess().getDataList().size());
+    }
+
+    @Test
+    void addInventory_error() {
+        var responseObserver = StreamRecorder.<CommerceResponse>create();
+        var requestObserver = inventoryGrpcService.addInventoryUnit(responseObserver);
+
+        var unit = InventoryUnit.getDefaultInstance();
+        doThrow(new InventoryException("error from service"))
+                .when(inventoryService)
+                .saveInventory(unit);
+
+        requestObserver.onNext(unit);
+
+        assertFalse(responseObserver.getValues().isEmpty());
+        assertEquals(1, responseObserver.getValues().size());
+
+        var commerceResponse = responseObserver.getValues().get(0);
+        assertNotNull(commerceResponse.getError());
+
+        var error = commerceResponse.getError();
+
+        assertNotNull(error.getMessage());
+    }
+
+    @Test
+    void removeInventory_success() {
+        var responseObserver = StreamRecorder.<CommerceResponse>create();
+        var requestObserver = inventoryGrpcService.removeInventoryUnit(responseObserver);
+
+        var unit = InventoryUnit.getDefaultInstance();
+
+        requestObserver.onNext(unit);
+
+        assertFalse(responseObserver.getValues().isEmpty());
+        assertEquals(1, responseObserver.getValues().size());
+
+        var commerceResponse = responseObserver.getValues().get(0);
+        assertNotNull(commerceResponse.getSuccess());
+
+        assertNotNull(commerceResponse.getSuccess().getMessage());
+    }
+
+    @Test
+    void removeInventory_error() {
+        var responseObserver = StreamRecorder.<CommerceResponse>create();
+        var requestObserver = inventoryGrpcService.removeInventoryUnit(responseObserver);
+
+        var unit = InventoryUnit.getDefaultInstance();
+        doThrow(new InventoryException("error from service"))
+                .when(inventoryService)
+                .removeInventory(unit);
+
+        requestObserver.onNext(unit);
+
+        assertFalse(responseObserver.getValues().isEmpty());
+        assertEquals(1, responseObserver.getValues().size());
+
+        var commerceResponse = responseObserver.getValues().get(0);
+        assertNotNull(commerceResponse.getError());
+
+        var error = commerceResponse.getError();
+
+        assertNotNull(error.getMessage());
+    }
+}
